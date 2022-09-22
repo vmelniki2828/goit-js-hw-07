8869ea17358eee1880534ade2729185ee921a861
vmelniki2828 <vmelniki2828@gmail.com>
2022-09-23 01:06:02 +0300

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('div.gallery');
+galleryContainer.insertAdjacentHTML("beforeend", createGalleryCard(galleryItems));
+
+function createGalleryCard(obj = {}) {
+    let galleryCards = '';
+    obj.forEach(({preview, original, description}) => 
+    {
+    const galleryCard= `
+<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+</a>
+</div>`;
+galleryCards += galleryCard;
+});
+return galleryCards;
+}
+
+galleryContainer.addEventListener('click', showImage);
+
+
+function showImage (event) {
+    event.preventDefault();
+    const selectedImage = event.target.dataset.source;
+    const instance = basicLightbox.create(
+        `<img class="gallery__image" src="${selectedImage}" />`, {closable: true}
+        );
+    instance.show();
+
+    document.addEventListener('keydown', closeOnEscape);
+    function closeOnEscape (event) {
+        if (event.key === "Escape") {    
+        instance.close();
+        document.removeEventListener('keydown', closeOnEscape);
+        }
+    }
+}
